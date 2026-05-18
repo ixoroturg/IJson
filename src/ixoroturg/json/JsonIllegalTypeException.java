9693cd144c629c862002b165e006e45a605652b5
ixoroturg <elukashin@meteo.ru>
2026-05-18 18:29:40 +0300

    Cleaned after merge

--- src/ixoroturg/json/JsonIllegalTypeException.java
+++
@@ -1,7 +0,0 @@
-package ixoroturg.json;
-
-public class JsonIllegalTypeException extends JsonException{
-  JsonIllegalTypeException(String description){
-    super(description);
-  }
-}
--- src/ixoroturg/json/JsonNoParentException.java
+++
@@ -1,7 +0,0 @@
-package ixoroturg.json;
-
-public class JsonNoParentException extends JsonException{
-  public JsonNoParentException(String description){
-    super(description);
-  }
-}
